f1ae977e7b60c1a1f555943f558b490d554cd241
Doğukan İNCİ <dogukaninci93@gmail.com>
2022-01-21 14:19:59 +0300

    Initial Commit

---
+++ Day-1-Variables.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodbye"
+var age = 38
+var population = 8000000
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+var pi = 3.141
+var awesome = true
+
+var score = 85
+str = "your score is \(score)"
+var results = "The best results are here \(str)"
+let taylor = "swift"
+
+let str3 = "Hello, playground"
+let year: Int = 1993
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+var price = 100_000
